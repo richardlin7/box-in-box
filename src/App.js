6b24ad1e9d04d470452ca15988538afec1b55a0a
Richard Lin <rlin7@student.gsu.edu>
2020-07-26 19:37:06 -0500

    parent can create and remove child box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-// make parent component that renders a box
-// when clicked inside, render box inside
-
 // Logic Process: clicked a box in parent =>
 // sent to onClick method =>
 // calls something that creates a new class with a box =>
 //(SHARES SIMILAR CODE AS PARENT AND REDUCED SIZE)
 
 //IE: root pass state down to child => child pass their state to their child etc.
+
+//create a method that will render box however many times its clicked
 import React from "react";
 
 class App extends React.Component {
@@ -15,30 +14,52 @@ class App extends React.Component {
       <div>
         <div
           onClick={this._onClick}
+          onContextMenu={this._onContextMenu}
           style={{
             position: "absolute",
             left: "50%",
             top: "50%",
             transform: "translate(-50%, -50%)",
-            height: this.state.height,
-            width: this.state.width,
+            height: this.height,
+            width: this.width,
             border: "1px solid black",
           }}
         />
-        {makeBox(this.state.child)}
+        {_makeBox(this.state.child, this.height, this.width)}
       </div>
     );
   }
   constructor() {
     super();
-    this.state = { height: 700, width: 1200 };
-    this._onClick = this._onClick.bind(this);
+    this.height = 700;
+    this.width = 1200;
+    this.state = { child: 0 };
   }
 
-  _onClick() {
-    console.log("clicked");
-  }
+  _onClick = (e) => {
+    e.preventDefault();
+    // console.log("clicked");
+    this.setState({ child: this.state.child + 1 });
+  };
+
+  _onContextMenu = (e) => {
+    e.preventDefault();
+    if (this.state.child !== 0) {
+      this.setState({ child: this.state.child - 1 });
+    }
+    return;
+  };
 }
+
+function _makeBox(value, height, width) {
+  for (var i = 0; i < value; i++) {
+    console.log("making box");
+    return <Box child={value} height={height} width={width} />;
+  }
+  return;
+}
+
+export default App;
 
 class Box extends React.Component {
   render() {
@@ -51,12 +72,11 @@ class Box extends React.Component {
             left: "50%",
             top: "50%",
             transform: "translate(-50%, -50%)",
-            height: this.props.height,
-            width: 1000,
+            height: this.props.height * 0.7,
+            width: this.props.width * 0.7,
             border: "1px solid black",
           }}
         />
-        {makeBox(this.state.child)}
       </div>
     );
   }
@@ -70,12 +90,3 @@ class Box extends React.Component {
     this.setState({ child: "" });
   }
 }
-
-function makeBox(x) {
-  if (x === 1) {
-    console.log("BOX MADE!");
-    return <Box />;
-  }
-  return;
-}
-export default App;
