180811d139c07b36e305b3c56453af529f7b32b1
Richard Lin <rlin7@student.gsu.edu>
2020-07-30 11:01:05 -0500

    recurses

--- src/App.js
+++ src/App.js
@@ -13,59 +13,11 @@ class App extends React.Component {
         style={{
           height: 700,
           width: 1400,
-          border: "1px solid black",
         }}
-        onClick={this._onClick}
-        onContextMenu={this._onContextMenu}
       >
-        {this.state.child.map(this._makeBox)}
+        <Box />
       </div>
     );
-  }
-
-  constructor() {
-    super();
-    this.state = { index: 0, child: [] };
-
-    this._makeBox = this._makeBox.bind(this);
-    this._onClick = this._onClick.bind(this);
-    this._onContextMenu = this._onContextMenu.bind(this);
-  }
-
-  _onClick() {
-    this.setState({
-      child: this.state.child.concat(""),
-      index: this.state.index + 1,
-    });
-    console.log("box made");
-  }
-
-  _onContextMenu(index) {
-    if (this.state.index !== 0) {
-      const child = this.state.child;
-
-      child.splice(index, 1);
-      this.setState({
-        child,
-        index: this.state.index - 1,
-      });
-      console.log("box removed");
-    }
-  }
-
-  _makeBox(index) {
-    // create recursion f6or makeBox method
-    if ((index = 1)) {
-      // when index(click) is 1, it returns the box once.
-      return (
-        <Box
-          onClick={() => this._onClick()}
-          onContextMenu={() => this._onContextMenu()}
-          makeBox={() => this._makeBox()}
-        />
-      );
-    }
-    return index * this._makeBox(index - 1); // returns box (index) many times
   }
 }
 
@@ -74,21 +26,31 @@ export default App;
 class Box extends React.Component {
   render() {
     return (
-      <div>
-        <div
-          style={{
-            width: "70%",
-            height: "70vh", // render at 70 percent smaller height
-            margin: "1%",
-            border: "1px solid black",
-          }}
-          onClick={this._onClick}
-          onContextMenu={this._onContextMenu}
-        />
-        {this.state.child.map(this.props.makeBox)}
+      <div
+        style={{
+          width: "70%",
+          height: "70%", // render at 70 percent smaller height
+          margin: "1%",
+          border: "1px solid black",
+        }}
+        onClick={this._onClick}
+        onContextMenu={this._onContextMenu}
+      >
+        {this.state.child.map((child, i) => {
+          return (
+            <Box
+              key={i}
+              // index={this.state.index === index}
+              child={child}
+              _onClick={() => this._onClick(i)}
+              _onContextMenu={() => this._onContextMenu(this.i)}
+            />
+          );
+        })}
       </div>
     );
   }
+
   constructor() {
     super();
     this.state = { index: 0, child: [] };
@@ -96,9 +58,10 @@ class Box extends React.Component {
     this._onClick = this._onClick.bind(this);
     this._onContextMenu = this._onContextMenu.bind(this);
   }
+
   _onClick() {
     this.setState({
-      child: this.state.child.concat(""),
+      child: this.state.child.concat(Box),
       index: this.state.index + 1,
     });
     console.log("box made");
